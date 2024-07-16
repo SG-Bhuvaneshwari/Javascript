559a4b269bfba769e74d0b2a54870f14fad9a799
SG-Bhuvaneshwari <sg.bhuvaneshwari98@gmail.com>
2024-07-16 14:58:00 +0530

    Add files via upload

---
+++ Javascript/Basic Try/Try.js
@@ -0,0 +1,7 @@
+var fruits = ["apple", "mango", "banana", "grapes","papali","jack"]
+
+// console.log (fruits.length)
+for(count=0; count<fruits.length; count=count+1)
+{
+console.log(fruits[count])
+}
